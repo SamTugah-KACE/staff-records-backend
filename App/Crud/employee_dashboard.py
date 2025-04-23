d7dfea1d5f1f938f72a6aaf0c67aa5c217068c7e
Anthony-Asamoah <anthonyasamoah48@gmail.com>
2025-04-23 11:23:30 +0000

    complete employee dashboard api

--- App/Crud/employee_dashboard.py
+++ App/Crud/employee_dashboard.py
@@ -1,97 +1,70 @@
-import logging
 from uuid import UUID
 
 from sqlalchemy.orm import Session, joinedload
 
-from Models.models import (
-    Employee, AcademicQualification, ProfessionalQualification, EmploymentHistory,
-    EmergencyContact, NextOfKin, EmployeePaymentDetail, SalaryPayment, PromotionRequest
-)
+from Models.models import Employee, SalaryPayment, PromotionRequest
 from Schemas.schemas import EmployeeDashboardSchema
 
 
 def get_employee_dashboard_info(db: Session, employee_id: UUID) -> EmployeeDashboardSchema:
     employee_query = (
-        db.query(
-            Employee,
-            AcademicQualification,
-            ProfessionalQualification,
-            EmploymentHistory,
-            EmergencyContact,
-            NextOfKin,
-            EmployeePaymentDetail,
-            SalaryPayment,
-            PromotionRequest,
-        )
-        # join & loads
+        db.query(Employee)
         .options(
             joinedload(Employee.rank),
             joinedload(Employee.employee_type),
             joinedload(Employee.department),
+            joinedload(Employee.dynamic_data),
+            joinedload(Employee.academic_qualifications),
+            joinedload(Employee.professional_qualifications),
+            joinedload(Employee.employment_history),
+            joinedload(Employee.emergency_contacts),
+            joinedload(Employee.next_of_kins),
+            joinedload(Employee.payment_details),
         )
-        # joins
-        .outerjoin(AcademicQualification, AcademicQualification.employee_id == Employee.id)
-        .outerjoin(ProfessionalQualification, ProfessionalQualification.employee_id == Employee.id)
-        .outerjoin(EmploymentHistory, EmploymentHistory.employee_id == Employee.id)
-        .outerjoin(EmergencyContact, EmergencyContact.employee_id == Employee.id)
-        .outerjoin(NextOfKin, NextOfKin.employee_id == Employee.id)
-        .outerjoin(EmployeePaymentDetail, EmployeePaymentDetail.employee_id == Employee.id)
-        .outerjoin(SalaryPayment, SalaryPayment.employee_id == Employee.id)
-        .outerjoin(PromotionRequest, PromotionRequest.employee_id == Employee.id)
-        # filters
         .filter(Employee.id == employee_id)
         .first()
     )
 
-    logging.info("employee query here")
-    logging.info(employee_query)
-    (
-        bio_data,
-        academic_qualifications,
-        professional_qualifications,
-        employment_histories,
-        emergency_contacts,
-        next_of_kins,
-        employee_payment_details,
-        salary_payments,
-        promotion_requests,
-    ) = employee_query
-
-    qualifications = dict()  # todo
-
-    employment_details = dict()
-    if bio_data.employee_type: employment_details['employee_type'] = dict(
-        type_code=bio_data.employee_type.type_code,
-        description=bio_data.employee_type.description,
-        default_criteria=bio_data.employee_type.default_criteria,
-    )
-    print("bio_data.rank here", bio_data.rank)
-    if bio_data.rank: employment_details['rank'] = dict(
-        id= bio_data.rank.id,
-        organization_id=bio_data.rank.organization_id,
-        name=bio_data.rank.name,
-        min_salary=bio_data.rank.min_salary,
-        max_salary=bio_data.rank.max_salary,
-        currency=bio_data.rank.currency,
-        conversion_info=bio_data.rank.conversion_info,
-    )
-    if bio_data.department: employment_details['department'] = dict(
-        id= bio_data.department.id,
-        organization_id=bio_data.department.organization_id,
-        name=bio_data.department.name,
-        branch_id=bio_data.department.branch_id,
-        department_head=bio_data.department.department_head_id
-    )
-    employment_details['dynamic_models'] = None
     response = dict(
-        bio_data=bio_data,
-        qualifications=qualifications,
-        employment_history=None,
-        emergency_contacts=None,
-        next_of_kins=None,
-        payment_details=None,
-        salary_payments=None,
-        promotion_requests=None,
-        employment_details=employment_details,
+        bio_data=employee_query,
+        qualifications=dict(
+            academic_qualifications=employee_query.academic_qualifications,
+            professional_qualifications=employee_query.professional_qualifications,
+        ),
+        employment_history=employee_query.employment_history,
+        emergency_contacts=employee_query.emergency_contacts,
+        next_of_kins=employee_query.next_of_kins,
+        payment_details=employee_query.payment_details,
+        salary_payments=db.query(SalaryPayment).filter(SalaryPayment.employee_id == employee_id).all(),
+        promotion_requests=db.query(PromotionRequest).filter(PromotionRequest.employee_id == employee_id).all(),
+        employment_details=dict(),
     )
+
+    # build employment details payload
+    if employee_query.employee_type: response['employment_details']['employee_type'] = dict(
+        type_code=employee_query.employee_type.type_code,
+        description=employee_query.employee_type.description,
+        default_criteria=employee_query.employee_type.default_criteria,
+    )
+    if employee_query.rank: response['employment_details']['rank'] = dict(
+        id=employee_query.rank.id,
+        organization_id=employee_query.rank.organization_id,
+        name=employee_query.rank.name,
+        min_salary=employee_query.rank.min_salary,
+        max_salary=employee_query.rank.max_salary,
+        currency=employee_query.rank.currency,
+        conversion_info=employee_query.rank.conversion_info,
+    )
+    if employee_query.department: response['employment_details']['department'] = dict(
+        id=employee_query.department.id,
+        organization_id=employee_query.department.organization_id,
+        name=employee_query.department.name,
+        branch_id=employee_query.department.branch_id,
+        department_head=employee_query.department.department_head_id
+    )
+    if employee_query.dynamic_data: response['employment_details']['dynamic_models'] = [dict(
+        data_category=i.data_category,
+        data=i.data,
+    ) for i in employee_query.dynamic_data]
+
     return EmployeeDashboardSchema(**response)
