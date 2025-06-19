cdcb48e15296977ce8d340c5f053d64031d87339
SamTugah-KACE <samuel.kusi-duah@gi-kace.gov.gh>
2025-06-19 04:32:01 +0000

    again-toenurl

--- App/Crud/sup_dependencies.py
+++ App/Crud/sup_dependencies.py
@@ -9,7 +9,7 @@ from database.db_session import get_async_db
 from Models.superadmin import SuperAdmin
 from Utils.sup_security import decode_jwt
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/superadmin/auth/token")
 
 settings = ProductionConfig()
 
